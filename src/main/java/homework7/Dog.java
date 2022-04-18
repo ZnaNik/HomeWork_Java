72fd0557ff57c30ced90a4fd6ebb52f9fa850ac7
ZnaNik <artedwork@gmail.com>
2022-04-18 12:49:03 +0300

    homework7

---
+++ src/main/java/homework7/Dog.java
@@ -0,0 +1,12 @@
+package homework7;
+
+public class Dog extends Animal {
+
+    public Dog(String name, int appetite){
+        super(name,appetite);
+        run_limit = 500;
+        swim_limit = 10;
+        Statistics.dogCount++;
+    }
+}
+
